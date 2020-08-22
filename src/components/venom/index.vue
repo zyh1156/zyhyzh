138519a6d3dde0c367daf736e45b4d419d03d69e
zyhzyh <115656771@qq.com>
2020-08-22 14:25:12 +0800

    相关优化

--- src/components/venom/index.vue
+++ src/components/venom/index.vue
@@ -1,10 +1,18 @@
 <template>
-    <section></section>
+  <section class="container">
+    <div class="text-center">
+      <div class="btn-group" role="group" aria-label="Basic example">
+        <button type="button" class="btn btn-success">Left</button>
+        <button type="button" class="btn btn-primary">Right</button>
+      </div>
+    </div>
+    <div></div>
+  </section>
 </template>
 <script>
 export default {
-    data(){
-        return{}
-    }
-}
+  data() {
+    return {};
+  },
+};
 </script>
